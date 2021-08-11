a6ccf4c28852766faf894495efd42a5b2b798c46
Gabriel Campanário <gabriel.camp96@hotmail.com>
2021-08-10 21:13:51 -0300

    crud finalizado

--- AplicaçãoRedeSocial/backend/src/controller/users.go
+++ AplicaçãoRedeSocial/backend/src/controller/users.go
@@ -82,9 +82,55 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Alterar usuario"))
+	parametros := mux.Vars(r)
+	var user model.User
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 8)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	request, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	if erro := json.Unmarshal(request, &user); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := db.Connect()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	_, erro = repository.UpdateUser(db, int(ID), user)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Exclui usuario"))
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 8)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := db.Connect()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	_, erro = repository.DeleteUser(db, int(ID))
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
--- AplicaçãoRedeSocial/backend/src/repository/user_repository.go
+++ AplicaçãoRedeSocial/backend/src/repository/user_repository.go
@@ -56,3 +56,60 @@ func GetUserById(db *sql.DB, ID int) (model.User, error) {
 
 	return user, nil
 }
+
+func UpdateUser(db *sql.DB, ID int, user model.User) (*sql.Result, error) {
+	result, erro := db.Query("select * from users u where u.id = ?", ID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer result.Close()
+
+	var userFounded model.User
+	for result.Next() {
+		if erro = result.Scan(
+			&userFounded.ID, &userFounded.Name, &userFounded.Nick, &userFounded.Email, &userFounded.Password, &userFounded.CreatedAt); erro != nil {
+			return nil, erro
+		}
+	}
+
+	if user.Name == "" {
+		user.Name = userFounded.Name
+	}
+	if user.Nick == "" {
+		user.Nick = userFounded.Nick
+	}
+	if user.Email == "" {
+		user.Email = userFounded.Email
+	}
+	if user.Password == "" {
+		user.Password = userFounded.Password
+	}
+
+	stmt, erro := db.Prepare("update users set name = ?, nick = ?, email = ?, password = ? where id = ?")
+	if erro != nil {
+		return nil, erro
+	}
+	defer stmt.Close()
+
+	resultStmt, erro := stmt.Exec(user.Name, user.Nick, user.Email, user.Password, ID)
+	if erro != nil {
+		return nil, erro
+	}
+
+	return &resultStmt, nil
+}
+
+func DeleteUser(db *sql.DB, ID int) (*sql.Result, error) {
+	stmt, erro := db.Prepare("delete from users where id = ?")
+	if erro != nil {
+		return nil, erro
+	}
+	defer stmt.Close()
+
+	result, erro := stmt.Exec(ID)
+	if erro != nil {
+		return nil, erro
+	}
+
+	return &result, nil
+}
